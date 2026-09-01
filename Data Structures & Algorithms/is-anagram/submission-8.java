01942fd5914442219936e96be1b32432f24f9788
Deepesh Sukhwani <105376191+DeepeshSukhwani@users.noreply.github.com>
2026-09-01 14:52:24 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        // Edge Case 
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char[] freq = new char[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+            freq[t.charAt(i)-'a']--;
+        }
+        for(int num : freq){
+            if(num!=0){
+                return false;
+            }
+        }
+        return true;
+
+
+        
+
+    }
+}
