de6732dfae190065c7a68950d093999a19739569
Deepesh Sukhwani <105376191+DeepeshSukhwani@users.noreply.github.com>
2026-09-14 18:51:34 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+     Set[] rows = new HashSet[9];
+     Set[] cols = new HashSet[9];
+     Set[] boxes = new HashSet[9];
+
+     for(int i=0;i<9;i++){
+        rows[i] = new HashSet<>();
+        cols[i] = new HashSet<>();
+        boxes[i] = new HashSet<>();
+     }
+
+     for(int i=0;i<board.length;i++){
+
+        for(int j=0;j<board.length;j++){
+            
+            if(board[i][j]=='.'){
+                continue;
+            }
+
+
+            if(!rows[i].add(board[i][j])){
+                return false;
+            }
+
+            else if(!cols[j].add(board[i][j])){
+                return false;
+            }
+
+            int box= (i/3) *3 + (j/3);
+
+            if(!boxes[box].add(board[i][j])){
+                return false;
+            }
+        }
+     }
+     return true;
+
+
+
+
+    }
+}
